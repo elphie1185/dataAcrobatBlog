ea9537b7c597960fc6b16e84fe1b82a4b5510d3c
elphie1185 <delphine_rabiller@yahoo.fr>
2022-06-11 09:52:47 +0100

    Initial commit

---
+++ databases.py
@@ -0,0 +1,32 @@
+
+##CONFIGURE TABLES
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(250), nullable=False)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+    def __init__(self, author, title, subtitle, date, body, img_url):
+        self.author = author
+        self.title = title
+        self.subtitle = subtitle
+        self.data = date
+        self.body = body
+        self.img_url = img_url
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "blog_users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
