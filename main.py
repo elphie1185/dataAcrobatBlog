553ba3e739667248c476546bd7fa1c9eec0b81c2
elphie1185 <delphine_rabiller@yahoo.fr>
2022-06-11 11:16:34 +0100

    Add postgresSQL

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from flask_gravatar import Gravatar
 from flask import abort
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "sqlite:///blog.db")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating="g", default="retro",
@@ -21,7 +21,7 @@ gravatar = Gravatar(app, size=100, rating="g", default="retro",
                     use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
