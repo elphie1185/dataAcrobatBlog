e91652231283c12de48e3056300e38a0cbeb7185
elphie1185 <delphine_rabiller@yahoo.fr>
2022-06-15 13:26:05 +0100

    Add contact form

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL, Email
+from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
 
@@ -33,6 +33,6 @@ class CommentForm(FlaskForm):
 
 class ContactForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email(granular_message=True)])
+    email = StringField("Email", validators=[DataRequired()])
     message = CKEditorField("Message", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
